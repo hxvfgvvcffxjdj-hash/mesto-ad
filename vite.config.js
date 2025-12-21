1cd417a5013f2692d34355e76cdfae90d50f9af6
kToYa&? <hxvfgvvcffxjdj@gmail.com>
2025-12-21 12:25:32 +0500

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: ".",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
+    },
+  },
+  server: {
+    open: true,
+    port: 5173,
+  },
+});
